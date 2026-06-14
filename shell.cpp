b90a513a0e543a17ef8c3a0d55b6a2eba3431c74
ELI <porcelain.kitten519@gmail.com>
2026-06-14 00:51:17 -0500

    updated the shell for basic execution of commands

--- shell.cpp
+++ shell.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void prompt() {
-  char *cmd = new char[128];
-  std::cout << "> ";
-  std::cin.getline(cmd, 128);
+void execCommand(char *cmd, char *argv[]) {
+  pid_t pid = fork();
+
+  if (pid < 0) {
+    std::cerr << "fork failed\n";
+  } else if (pid == 0) {
+    execvp(cmd, argv);
+    std::cerr << "execvp failed\n";
+    exit(1);
+  }
+  waitpid(pid, NULL, 0);
 }
 
-int main() {
-  prompt();
+int main(int argc, char *argv[]) {
+  char *cmd = new char;
+  char *args = new char[32];
+  
+  while (1) {
+    std::cout << "> ";
+    std::cin.getline(cmd, 128);
+    execCommand(cmd, argv);
+  }
+
+  delete cmd;
+
   return 0;
 }
