54b8c6f99887875ad39d93d2d5dbd1783b3e1f4e
ELI <porcelain.kitten519@gmail.com>
2026-06-17 18:40:52 -0500

    using vector to parse the command

--- shell.cpp
+++ shell.cpp
@@ -1,9 +1,18 @@
+/*
+  basic replica of bash shell
+  to do: need to convert parsed vector into char array to be able to pass into execvp
+ */
+
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <vector>
+#include <cstring>
 
+// function used to execute commands given
+// right now can only execute first command and not arguments
 void execCommand(char *cmd, char *argv[]) {
   pid_t pid = fork();
 
@@ -17,13 +26,26 @@ void execCommand(char *cmd, char *argv[]) {
   waitpid(pid, NULL, 0);
 }
 
+//function used to parse the command initially typed into a vector
+std::vector<char*> parseCommand(char *cmd) {
+  char *token = std::strtok(cmd, " ");
+  std::vector<char *> random_vec;
+  while (token != nullptr) {
+    random_vec.push_back(token);
+    token = std::strtok(nullptr, " ");
+  }
+
+  return random_vec;
+}
+
 int main(int argc, char *argv[]) {
-  char *cmd = new char;
-  char *args = new char[32];
+  char *cmd = new char[128];
   
   while (1) {
     std::cout << "> ";
     std::cin.getline(cmd, 128);
+    std::vector<char *> parsed_cmd = parseCommand(cmd);
+    //HERE: convert vector to char array
     execCommand(cmd, argv);
   }
 
