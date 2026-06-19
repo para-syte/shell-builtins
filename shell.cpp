e7d44524ef5f2c5824ec0dcb77b116084f3f202f
ELI <porcelain.kitten519@gmail.com>
2026-06-18 23:58:53 -0500

    converted vect into char array and passed into execCommand to be able to execute commands with args

--- shell.cpp
+++ shell.cpp
@@ -13,20 +13,20 @@
 
 // function used to execute commands given
 // right now can only execute first command and not arguments
-void execCommand(char *cmd, char *argv[]) {
+void execCommand(char *cmd, char *ptr[]) {
   pid_t pid = fork();
 
   if (pid < 0) {
     std::cerr << "fork failed\n";
   } else if (pid == 0) {
-    execvp(cmd, argv);
+    execvp(cmd, ptr);
     std::cerr << "execvp failed\n";
     exit(1);
   }
   waitpid(pid, NULL, 0);
 }
 
-//function used to parse the command initially typed into a vector
+// function used to parse the command initially typed into a vector
 std::vector<char*> parseCommand(char *cmd) {
   char *token = std::strtok(cmd, " ");
   std::vector<char *> random_vec;
@@ -35,21 +35,25 @@ std::vector<char*> parseCommand(char *cmd) {
     token = std::strtok(nullptr, " ");
   }
 
+  // null terminate vector before converting
+  random_vec.push_back(nullptr);
+
   return random_vec;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
   char *cmd = new char[128];
   
   while (1) {
     std::cout << "> ";
     std::cin.getline(cmd, 128);
     std::vector<char *> parsed_cmd = parseCommand(cmd);
-    //HERE: convert vector to char array
-    execCommand(cmd, argv);
+    // HERE: convert vector to char array
+    auto *ptr = parsed_cmd.data();
+    execCommand(ptr[0], ptr);
   }
 
-  delete cmd;
+  delete[] cmd;
 
   return 0;
 }
